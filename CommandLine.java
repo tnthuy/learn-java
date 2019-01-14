ccdb4b438c2a0f06d9e1b9c214a23eabf1500c47
tnthuy <thuy140583@gmail.com>
2019-01-14 21:59:58 +0700

    The first commit

---
+++ CommandLine.java
@@ -0,0 +1,8 @@
+public class CommandLine {
+    public static void   main (String args[]) {
+        for (int i=1; i<args.length;i++) {
+            System.out.println("a["+ i +"]:"+args[i]);
+        }
+    }
+
+}
